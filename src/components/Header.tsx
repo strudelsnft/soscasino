84f657e67950413b92b66f43fb00a3f016e0e09b
strudelsnft <103766813+strudelsnft@users.noreply.github.com>
2023-11-30 16:34:37 -0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,11 +13,11 @@ export const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
         </div>
         <div style={{ display: 'flex', gap: '20px', alignItems: 'center' }}>
           <div className={styles.externalLinks}>
-            <a href="https://github.com/gamba-labs/gamba" target="_blank" rel="noreferrer">
-              Github
+            <a href="https://t.me/strudelsonsolana" target="_blank" rel="noreferrer">
+              Telegram
             </a>
-            <a href="https://explorer.gamba.so" target="_blank" rel="noreferrer">
-              Stats
+            <a href="https://birdeye.so/token/2mnGSkXH1h6x5qmhwoQzAZDKa83vnRf8wNkNWVbdv7w5/7H5ijbPvBqRVBoDMtJhgQetxS9e2KFikP18JrabkctAS?chain=solana" target="_blank" rel="noreferrer">
+              Birdeye
             </a>
           </div>
           {children}
